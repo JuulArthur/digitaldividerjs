228f509250234c5fff3932768f79740037dbeda9
Juul Rudihagen <juul rudihagen@mac-juulrudihagen.local>
2014-06-18 23:05:27 +0200

    Next step is to make sure numbers after the one dragged doesn't dissepear and that you loose when on red

--- www/js/game.js
+++ www/js/game.js
@@ -9,6 +9,7 @@ var width = 800,
 	minRandom = 0;
 	dragging = false;
 	moveBomb = null;
+	indexOfMovedBomb = -1;
 	mouseX = 0;
 	mouseY = 0;
 
@@ -28,6 +29,7 @@ var drawBomb = function(){
 		img : new Image(),
 		x : 0,
 		y : 200,
+		number : Math.floor(Math.random()*(20-1+1)+1),
 		dragged : false
 	};
 
@@ -35,7 +37,7 @@ var drawBomb = function(){
 	
 	bomb.img.onload = function() {
         ctx.drawImage(bomb.img, 0, 200);
-        ctx.fillText("1", 0, 200);
+        ctx.fillText(""+bomb.number, 0, 200);
     };
 
     bomb.img.src = "img/Twirl1.png";
@@ -46,19 +48,23 @@ var drawBomb = function(){
 
 var moveBombs = function(){
 	var bomb;
-	this.bombs.forEach(function(bomb){
-		if (bomb.x>=0 && !bomb.dragged){
+	for(i=0; i<bombs.length; i++){
+		bomb = bombs[i];
+		if (bomb.x>=800){
+			bombs.splice(i,1);
+		}
+		else if (bomb.x>=0 && !bomb.dragged){
 			bomb.x = bomb.x+2;
 			this.ctx.drawImage(bomb.img, bomb.x, bomb.y);
-			this.ctx.fillText("7", bomb.x+25, bomb.y+55);
+			this.ctx.fillText(""+bomb.number, bomb.x+25, bomb.y+55);
 		}
 		else if (bomb.x>=0 && bomb.dragged){
 			if(!dragging){
-				console.log("what");
+				//console.log("what");
 				bomb.dragged = false;
 			}
 		}
-	});
+	}
 };
 
 var background = new (function(){
@@ -82,24 +88,72 @@ var myDown = function(e){
 	dragging = true;
 	closestDistance = 99999;
 	var distance = 0;
-	mouseX = e.layerX;
-	mouseY = e.pageY;
-	bombs.forEach(function(bomb){
-		distance = (bomb.x-mouseX)^2+(bomb.y-mouseY)^2;
+	//mouseX = e.layerX;
+	//mouseY = e.pageY;
+	console.log(bombs);
+	console.log("halla");
+	for(i=0; i<bombs.length; i++){
+		console.log("bombX: " + bombs[i].x)
+		console.log("mouseX: " + mouseX)
+		console.log(Math.pow((bombs[i].y-mouseY),2))
+		distance = Math.sqrt(Math.pow((bombs[i].x-mouseX),2)+Math.pow((bombs[i].y-mouseY),2));
+		console.log("distance: "+distance)
 		if(distance < closestDistance){
 			closestDistance = distance;
-			moveBomb = bomb;
+			moveBomb = bombs[i];
+			indexOfMovedBomb = i;
 		}
-	moveBomb.dragged = true;
-	});
+		moveBomb.dragged = true;
+	};
 };
+
+var inFirstField = function(){
+	return (moveBomb.x>60 && moveBomb.x<200 && moveBomb.y>0 && moveBomb.y<150);
+}
+
+var inSecondField = function(){
+	return (moveBomb.x>350 && moveBomb.x<500 && moveBomb.y>0 && moveBomb.y<150);
+}
+
+var inThirdField = function(){
+	return (moveBomb.x>60 && moveBomb.x<200 && moveBomb.y>330 && moveBomb.y<480);
+}
+
+var inFourthField = function(){
+	return (moveBomb.x>350 && moveBomb.x<500 && moveBomb.y>330 && moveBomb.y<480);
+}
+
+var calculateField = function(fieldNumber){
+	if(moveBomb.number % fieldNumber == 0){
+		console.log("Hurray!");
+	}
+	else{
+		console.log("ya lost m8");
+	}
+}
 
 var myUp = function(){
 	dragging = false;
-	console.log("jada")
+	if(inFirstField()){
+		calculateField(1);
+		bombs.splice(indexOfMovedBomb,1);
+	}
+	else if(inSecondField()){
+		calculateField(2);
+		bombs.splice(indexOfMovedBomb,1);
+	}
+	else if(inThirdField()){
+		calculateField(5);
+		bombs.splice(indexOfMovedBomb,1);
+	}
+	else if(inFourthField()){
+		calculateField(7);
+		bombs.splice(indexOfMovedBomb,1);
+	}
+	//console.log("jada")
 	if(moveBomb!=null){
 		moveBomb.dragged = false;
-		console.log(moveBomb.x)
+		//console.log(moveBomb.x)
 		moveBomb = null;
 	}
 };
@@ -109,11 +163,26 @@ var dragBomb = function(){
 		//console.log(mouseX);
 		//moveBomb.x = mouseX+c.offsetLeft;
 		//moveBomb.y = mouseY+c.offsetRight;
-		moveBomb.x = moveBomb.x - 5;
+		moveBomb.x = mouseX - 25;
+		moveBomb.y = mouseY - 55;
 		this.ctx.drawImage(moveBomb.img, moveBomb.x, moveBomb.y);
-		this.ctx.fillText("7", moveBomb.x+25, moveBomb.y+55);
+		this.ctx.fillText(""+moveBomb.number, moveBomb.x+25, moveBomb.y+55);
 	}
 };
+
+function setMousePosition(e)
+{
+    if(e.offsetX) {
+        this.mouseX = e.offsetX;
+        this.mouseY = e.offsetY;
+    }
+    else if(e.layerX) {
+        this.mouseX = e.layerX;
+        this.mouseY = e.layerY;
+    }
+
+    console.log(this.mouseY);
+}
 
 c.onmousedown = myDown;
 c.onmouseup = myUp;
